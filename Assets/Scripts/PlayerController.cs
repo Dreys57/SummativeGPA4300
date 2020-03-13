f6d0a91b10b5ed87b94016dc8f3a80f38f3d890f
Vincent <skriosdreys@gmail.com>
2020-03-13 10:50:52 +0100

    Player sticks to moving platform now

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D body;
+    private Transform transform_;
 
     [SerializeField] private float speed = 10.0f;
 
@@ -14,6 +15,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        transform_ = GetComponent<Transform>();
     }
 
     private void FixedUpdate()
@@ -52,5 +54,21 @@ public class PlayerController : MonoBehaviour
     private void FlipUpsideDown()
     {
         transform.Rotate(180.0f, 0f, 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            transform_.parent = other.gameObject.transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            transform_.parent = null;
+        }
     }
 }
