6c2cad894eee7efcdc793eeab7063b7d70db1c30
Vincent <skriosdreys@gmail.com>
2020-03-13 09:33:33 +0100

    Added player controller script and gravity change

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    [SerializeField] private float speed = 10.0f;
+
+    private float movementInputDirection;
+   
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+    }
+
+
+    void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        movementInputDirection = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            ChangeGravity();
+        }
+    }
+
+    private void ApplyMovement()
+    {
+        body.velocity = new Vector2(speed * movementInputDirection, body.velocity.y);
+    }
+
+    private void ChangeGravity()
+    {
+        body.gravityScale = -1 * body.gravityScale;
+    }
+}
