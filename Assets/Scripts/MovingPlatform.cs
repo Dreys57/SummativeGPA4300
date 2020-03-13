c9a3f8f46eb1c6a0f35860453fb36608eda6f79f
Vincent <skriosdreys@gmail.com>
2020-03-13 10:37:06 +0100

    Adding moving platform prefab and script

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private Transform pos1;
+    [SerializeField] private Transform pos2;
+    [SerializeField] private Transform startPos;
+    
+    [SerializeField] private float speed;
+
+    private Vector3 nextPos;
+    
+    void Start()
+    {
+        nextPos = startPos.position;
+    }
+
+    
+    void Update()
+    {
+        if (transform.position == pos1.position)
+        {
+            nextPos = pos2.position;
+        }
+        if (transform.position == pos2.position)
+        {
+            nextPos = pos1.position;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(pos1.position, pos2.position);
+    }
+}
