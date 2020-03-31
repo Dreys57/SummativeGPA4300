72996f9ce5c855c757604415f2697b961dd381b3
Dreys57 <skriosdreys@gmail.com>
2020-03-31 13:22:09 +0200

    Created spawn point and script for the mirror player and put it in a prefab

---
+++ Assets/Scripts/MirrorInstantiation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MirrorInstantiation : MonoBehaviour
+{
+    [SerializeField] private Transform mirrorSpawn;
+
+    [SerializeField] private GameObject mirrorPlayerPrefab;
+
+    [SerializeField] private GameObject Player;
+
+    private GameObject mirrorPlayer;
+    
+    private bool hasSpawnedMirror = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && hasSpawnedMirror == false)
+        {
+            mirrorPlayer = Instantiate(mirrorPlayerPrefab, mirrorSpawn);
+            mirrorPlayer.GetComponent<Rigidbody2D>().gravityScale = -Player.GetComponent<Rigidbody2D>().gravityScale;
+            
+            if (mirrorPlayer.GetComponent<Rigidbody2D>().gravityScale > 0.1f)
+            {
+                mirrorPlayer.transform.Rotate(180.0f, 0f,0f);
+            }
+            
+            hasSpawnedMirror = true;
+        }
+    }
+}
