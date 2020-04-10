43f7b59c54cf963291ba4275abb05b2ebe179d1d
Solange <sol.sch1990@gmail.com>
2020-04-10 12:11:44 +0200

    Add in script MenuManager functions for load Scene + functions activate and desactivate panels + Add panel Menu and panel Credits

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject inventoryPanel;
+    [SerializeField] private GameObject panelMenu;
+    [SerializeField] private GameObject panelCredits;
+    [SerializeField] private GameObject panelMenuPause;
 
     void Start()
     {
@@ -38,5 +42,47 @@ public class MenuManager : MonoBehaviour
         inventoryPanel.SetActive(true);
 
         Time.timeScale = 0;
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        Time.timeScale = 1;
+    }
+
+    public void ActivatePanelCredits()
+    {
+        panelCredits.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void DesactivatePanelCredits()
+    {
+        panelCredits.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void ActivateMenuPause()
+    {
+        panelMenuPause.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void DesactivateMenuPause()
+    {
+        panelMenuPause.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void ActivatePanelMenu()
+    {
+        panelMenu.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void DesactivatePanelMenu()
+    {
+        panelMenu.gameObject.SetActive(false);
+        Time.timeScale = 1;
     }
 }
